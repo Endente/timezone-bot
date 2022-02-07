7ce72985e0432e553eab5dc397128808cc2d40cf
stefco05 <s_cocco@icloud.com>
2022-02-07 19:54:34 +0100

    Improved logging of actions and requests

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./config.json')
 const test = require('./test.js')
 function checkCommand(message){
     if(message.content == "!test"){
+        console.log(`Running "${message}"; Requested by "${message.author.username}" at "${message.id}"`);
         test(message);
     }
 }
