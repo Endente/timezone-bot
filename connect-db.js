a1aeb2f3d521e89acc29834c5642bfcfe5a9e867
stefco05 <s_cocco@icloud.com>
2022-02-07 20:44:19 +0100

    Added db login support

---
+++ connect-db.js
@@ -0,0 +1,11 @@
+const config = require('./config.json')
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(`postgres://${config.dbUser}:${config.dbPass}@${config.dbAddress}:${config.dbPort}/${config.dbServer}`)
+async function testConnection(){
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}}
+testConnection();
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./config.json')
 const test = require('./test.js')
+const connectdb = require('./connect-db.js')
 function checkCommand(message){
     if(message.content == "!test"){
         console.log(`Running "${message}"; Requested by "${message.author.username}" at "${message.id}" on Channel "${message.channelId}"`);
         test(message);
     }
 }
+connectdb
 client.on('ready', () => {
     console.log('Bot is now online.')
     client.on("messageCreate", (message) => {
