bac3b3bbcacd18e72867db112aabfc7e9fc5792a
stefco05 <s_cocco@icloud.com>
2022-02-07 19:41:37 +0100

    Improved Command Search

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./config.json')
 const test = require('./test.js')
-function checkCommand(string){
-    if(string.content == "!test"){
-        test(string);
+function checkCommand(message){
+    if(message.content == "!test"){
+        test(message);
     }
 }
 client.on('ready', () => {
     console.log('Bot is now online.')
     client.on("messageCreate", (message) => {
-        if(message.content == "!test"){
+        if(message.content.startsWith(config.prefix)){
             checkCommand(message);
         }
     })
