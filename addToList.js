d0ae38803fa7669bc0877b47a5d12cf54b33f545
stefco05 <s_cocco@icloud.com>
2022-08-20 00:41:08 +0100

    Updated progress

--- addToList.js
+++ addToList.js
@@ -4,3 +4,5 @@ const { Sequelize } = require('sequelize');
 const client = require("./index")
 const sequelize = new Sequelize('database', '', '', {dialect:'sqlite', storage:'sqlite.db'})
 //const sequelize = new Sequelize(`postgres://${config.dbUser}:${config.dbPass}@${config.dbAddress}:${config.dbPort}/${config.dbServer}`)
+
+//DB Scheme: ServerID, UserID, 
