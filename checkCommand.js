0501604f2e3206898cae4fe41f62311613a776a4
stefco05 <s_cocco@icloud.com>
2022-08-20 21:37:46 +0100

    Added "addToList.js" support

--- checkCommand.js
+++ checkCommand.js
@@ -2,6 +2,7 @@ const config = require('./config.json')
 const test = require('./test.js')
 const connectdb = require('./connect-db.js')
 const dbstatus = require('./dbstatus.js')
+const addToList = require(`./addToList.js`)
 function logToConsole(message){
     console.log(`Running "${message}"; Requested by "${message.author.username}" at "${message.id}" on Channel "${message.channel.name}"`);
 }
@@ -16,6 +17,9 @@ module.exports = (message) => {
         }else if(message.content == `${config.prefix}dbstatusprivate`){
             logToConsole(message);
             connectdb;
+        }else if(message.content == `${config.prefix}add`){
+            logToConsole(message);
+            addToList(message);
         }
     }catch(e){}
 }
